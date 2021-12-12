d9689fffb2834301f15555016d8dbd0becf3bd95
Daniel Abdelsamed <daniel@abdelsamed.com>
2021-12-12 17:26:27 -0500

    Added extra note for other players

--- src/main/java/com/danielabdelsamed/ServerPlugin/ServerListener.java
+++ src/main/java/com/danielabdelsamed/ServerPlugin/ServerListener.java
@@ -6,6 +6,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class ServerListener implements Listener {
     @EventHandler
     public void OnChat(AsyncPlayerChatEvent event) {
@@ -18,7 +22,18 @@ public class ServerListener implements Listener {
         // Find that player's color based on the hash of their unique ID
         ChatColor color = colors[Math.abs(player.getUniqueId().hashCode()) % colors.length];
 
+        Set<Player> players = new HashSet<>(event.getPlayer().getServer().getOnlinePlayers());
+        players.remove(event.getPlayer());
+
+        if (players.size() == 0) {
+            event.setFormat("[" + color + player.getDisplayName() + ChatColor.RESET  + "] " + event.getMessage() + (event.getMessage().endsWith(".") ? "" : ".") + " I also "+ ChatColor.RED +"looooooove" + ChatColor.RESET + " cum.");
+            return;
+        }
+
+        Player otherPlayer = players.toArray(new Player[0])[new Random().nextInt(players.size())];
+        ChatColor otherPlayerColor = colors[Math.abs(otherPlayer.getUniqueId().hashCode()) % colors.length];
+
         // Format the message with their color
-        event.setFormat("[" + color + player.getDisplayName() + ChatColor.RESET  + "] " + event.getMessage());
+        event.setFormat("[" + color + player.getDisplayName() + ChatColor.RESET  + "] " + event.getMessage() + (event.getMessage().endsWith(".") ? "" : ".") + " I also "+ ChatColor.RED +"looooooove " + otherPlayerColor + otherPlayer.getDisplayName() + ChatColor.RESET + "'s"  + " cum.");
     }
 }
